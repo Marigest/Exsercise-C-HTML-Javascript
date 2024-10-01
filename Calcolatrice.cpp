bfbadf5dfc8f30f3203dcf0017aa2942af307c51
Marigest <marigest.sadiku@itctosi.va.it>
2024-10-01 17:07:42 +0000

    Modification

--- Calcolatrice.cpp
+++ Calcolatrice.cpp
@@ -3,133 +3,75 @@ using namespace std;
 
 class Calcolatrice {
 private:
-
-int a, b;
-
+    int a, b;
 public:
-
 void Numeri(int primo, int secondo) {
-
-a = primo;
-
-b = secondo;
-
+    a = primo;
+    b = secondo;
 }
 
 int somma() {
-
-return a + b;
-
+    return a + b;
 }
 
 int sottrazione() {
-
-return a - b;
-
+    return a - b;
 }
 
 int moltiplicazione() {
-
-return a * b;
-
+    return a * b;
 }
 
 int divisione() {
-
-return a / b;
-
+    return a / b;
 }
 
 int mcd() {
-
 int x = a, y = b;
-
-while (y != 0) {
-
-int temp = y;
-
-y = x % temp;
-
-x = temp;
-
-}
-
+    while (y != 0) {
+    int temp = y;
+        y = x % temp;
+        x = temp;
+    }
 return x;
-
 }
 
 int mcm() {
-
 return (a * b) / mcd();
-
 }
 
 };
 
-
 int main() {
-
 Calcolatrice calc;
-
 int a, b;
-
 char operazione;
-
-cout<<"Inserisci l'operazione (+, -, *, /): ";
-
-cin>>operazione;
-
-cout<<"Inserisci il primo numero: ";
-
-cin>>a;
-
-cout<<"Inserisci il secondo numero: ";
-
-cin>>b;
-
-calc.Numeri(a, b);
-
-
+    cout<<"Inserisci l'operazione (+, -, *, /): ";
+        cin>>operazione;
+    cout<<"Inserisci il primo numero: ";
+        cin>>a;
+    cout<<"Inserisci il secondo numero: ";
+        cin>>b; 
+    calc.Numeri(a, b);
 switch (operazione) {
-
-case '+':
-
-cout<<"Risultato: "<<calc.somma()<<endl;
-
-break;
-
-case '-':
-
-cout<<"Risultato: "<<calc.sottrazione()<<endl;
-
-break;
-
-case '*':
-
-cout<<"Risultato: "<<calc.moltiplicazione()<<endl;
-
-break;
-
-case '/':
-
-if (b != 0)
-
-cout<<"Risultato: "<<calc.divisione()<<endl;
-
-else
-
-cout<<"Errore: divisione per zero!"<<endl;
-
-break;
-
-
+    case '+':
+        cout<<"Risultato: "<<calc.somma()<<endl;
+            break;
+    case '-':
+        cout<<"Risultato: "<<calc.sottrazione()<<endl;
+            break;
+    case '*':
+        cout<<"Risultato: "<<calc.moltiplicazione()<<endl;
+            break;
+    case '/':
+        if (b != 0){
+            cout<<"Risultato: "<<calc.divisione()<<endl;
+        }else{
+            cout<<"Errore: divisione per zero!"<<endl;
+        }
+                break;
 }
-
-cout<<"MCD: "<<calc.mcd()<<endl;
-
-cout<<"MCM: "<<calc.mcm()<<endl;
-
-
-return 0;
-
+    cout<<"MCD: "<<calc.mcd()<<endl;
+    cout<<"MCM: "<<calc.mcm()<<endl;
+    return 0;
 }
