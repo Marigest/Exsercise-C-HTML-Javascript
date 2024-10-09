ffcd4b6d064c7abb089a1be1ea0c43dc665e1b6c
Marigest <marigest.sadiku@itctosi.va.it>
2024-10-09 10:04:54 +0000

    Modification

--- Lampadina.cpp
+++ Lampadina.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
-//da finire
-int main(){
-    
+
+class Lampadine {
+   private:
+       bool stato;
+   public:
+       Lampadine(){
+           stato=false;
+       }
+       void interruttore(){
+           if(stato==true){
+               stato = false;
+           }else{
+               stato = true;
+           }
+       }
+       bool vedi(){
+           return stato;
+       }
+};
+int main() {
+   srand(time(0));
+   Lampadine x, y, z;
+   int temp;
+   for(int i=0;i<1000;i++){
+       temp = rand() % 3;
+       switch(temp){
+           case 0: x.interruttore();
+               break;
+           case 1: y.interruttore();
+               break;
+           case 2: z.interruttore();
+               break;
+       }
+   }
+   cout<<"Stato lampadina 1: "<<(x.vedi() ? "Accesa" : "Spenta")<<endl;
+   cout<<"Stato lampadina 2: "<<(y.vedi() ? "Accesa" : "Spenta")<<endl;
+   cout<<"Stato lampadina 3: "<<(z.vedi() ? "Accesa" : "Spenta")<<endl;
 }
