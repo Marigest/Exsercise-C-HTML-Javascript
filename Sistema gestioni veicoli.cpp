5044aebb710266189b84ecb986d2a58ed7c812e6
Marigest <marigest.sadiku@itctosi.va.it>
2024-10-10 16:09:32 +0000

    Modification

--- Sistema gestioni veicoli.cpp
+++ Sistema gestioni veicoli.cpp
@@ -22,7 +22,7 @@ class Auto:public Veicolo{
         int numeroPorte;
     public:
         Auto(string mar, string mod, int an, int num):Veicolo(mar, mod, an){
-            numeroPorte=num;
+            numeroPorte=num; 
         }
         void mostra_dettagli(){
             cout<<"La marca dell'auto è "<<marca<<" di modello "<<modello<<" di anno "<<anno<<" con "<<numeroPorte<<" porte"<<endl;
@@ -33,12 +33,20 @@ class Moto:public Veicolo{
     private:
         bool haBauletto;
     public:
-        Moto(string mar, string mod, int a, bool baule):Veicolo(mar, mod, an){
-            
+        Moto(string mar, string mod, int a, bool baule):Veicolo(mar, mod, a){
+            baule=haBauletto;
         }
-}
+        void mostra_dettagli(){
+            cout<<"La marca della moto è "<<marca<<" di modello "<<modello<<" di anno "<<anno;
+            if(haBauletto){
+                cout<<" con bauletto"<<endl;
+            }else{
+                cout<<" senza bauletto"<<endl;
+            }
+        }
+};
 
 int main(){
-    Veicolo registro=Veicolo("BMW", "M3 E46", 2002);
+    Veicolo registro=Veicolo("BMW", "M3 E46", 2001);
     registro.mostra_dettagli();
 }
