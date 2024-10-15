f464e55129014c7dd14e60885c74595c8af1197f
Marigest <marigest.sadiku@itctosi.va.it>
2024-10-15 06:49:52 +0000

    Modification

--- Esercizi/Sistema gestioni veicoli.cpp
+++ Esercizi/Sistema gestioni veicoli.cpp
@@ -12,9 +12,6 @@ class Veicolo{
         modello=mod;
         anno=an;
     }
-    void mostra_dettagli(){
-        cout<<"La marca del veicolo è "<<marca<<" di modello "<<modello<<" di anno "<<anno<<"."<<endl;
-    }
 };
 
 class Auto:public Veicolo{
@@ -27,6 +24,9 @@ class Auto:public Veicolo{
         void mostra_dettagli(){
             cout<<"La marca dell'auto è "<<marca<<" di modello "<<modello<<" di anno "<<anno<<" con "<<numeroPorte<<" porte."<<endl;
         }
+        float calcolabollo(){
+        return (2024-anno)*1.15;
+    }
 };
 
 class Moto:public Veicolo{
@@ -44,13 +44,34 @@ class Moto:public Veicolo{
                 cout<<" senza bauletto."<<endl;
             }
         }
+        float calcolabollo(){
+        return ((2024-anno)*1.15)-(((2024-anno)*1.5)*0.20);
+    }
+};
+
+class Camion:public Veicolo{
+    private:
+        int capacita_carico;
+    public:
+        Camion(string mar, string mod, int a, int capacita):Veicolo(mar, mod, a){
+            capacita=capacita_carico;
+        }
+        void mostra_dettagli(){
+            cout<<"La marca del camion è "<<marca<<" di modello "<<modello<<" di anno "<<anno<<" con una capacità di "<<capacita_carico<<"."<<endl;
+        }
+        float calcolabollo(){
+        return ((2024-anno)*1.15)+(capacita_carico*0.7);
+        }
 };
 
 int main(){
-    Veicolo registro=Veicolo("Lamborghini", "Gallardo", 2003);
-    Auto reg=Auto("BMW", "M3 E46", 2001, 3);
-    Moto r=Moto("Kawasaki", "Ninja H2R", 2023, false);
+    Auto registro=Auto("BMW", "M3 E46", 2001, 3);
+    Moto reg=Moto("Kawasaki", "Ninja H2R", 2023, false);
+    Camion r=Camion("Iveco", "S-eWay", 2019, 100);
     registro.mostra_dettagli();
     reg.mostra_dettagli();
     r.mostra_dettagli();
+    cout<<"Il bollo del veicolo è di "<<registro.calcolabollo()<<endl;
+    cout<<"Il bollo della moto è di "<<reg.calcolabollo()<<endl;
+    cout<<"Il bollo del camion è di "<<r.calcolabollo()<<endl;
 }
