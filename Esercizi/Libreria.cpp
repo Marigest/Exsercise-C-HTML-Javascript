f3fe0938f6dc41b3acd1a6c04109f0c69ed39f66
Marigest <marigest.sadiku@itctosi.va.it>
2024-10-17 16:14:19 +0000

    Modification

--- Esercizi/Libreria.cpp
+++ Esercizi/Libreria.cpp
@@ -2,72 +2,62 @@
 #include <string>
 using namespace std;
 
-class Libreria {
-protected:
-    string titolo;
-    int annoPubblicazione;
-
-public:
-    Libreria(string tit, int anPub) : titolo(tit), annoPubblicazione(anPub) {}
-
-    void mostraDettagli() {
-        cout << "Titolo: " << titolo << ", Anno di Pubblicazione: " << annoPubblicazione << endl;
-    }
-
-    bool isAntico() {
-        int annoCorrente = 2023;
-        return (annoCorrente - annoPubblicazione) >= 50;
-    }
-
-    int calcolaAnni() {
-        int annoCorrente = 2023;
-        return annoCorrente - annoPubblicazione;
-    }
-};
-
-class Libro : public Libreria {
-private:
-    int numeroPagine;
-    string genere;
-
-public:
-    Libro(string tit, int anPub, int pagine, string gen) : Libreria(tit, anPub), numeroPagine(pagine), genere(gen) {}
-
-    bool verificaLunghezza() {
-        return numeroPagine > 300;
-    }
-
-    bool eFantasy() {
-        return genere == "Fantasy";
-    }
-
-    void calcolaTempoLettura() {
-        int tempoLettura = numeroPagine / 30;
-        cout << "Tempo stimato di lettura: " << tempoLettura << " ore" << endl;
-    }
-};
-
-class Rivista : public Libreria {
-private:
-    int numeroArticoli;
-    string categoria;
-
-public:
-    Rivista(string tit, int anPub, int articoli, string cat) : Libreria(tit, anPub), numeroArticoli(articoli), categoria(cat) {}
-
-    bool haMoltiArticoli() {
-        return numeroArticoli > 10;
-    }
-
-    bool eDiCategoria(string cat) {
-        return categoria == cat;
-    }
-
-    void mostraArticoli() {
-        for (int i = 1; i <= numeroArticoli; i++) {
-            cout << "Articolo " << i << endl;
+class Libreria{
+    protected:
+        string titolo;
+        int annoPubblicazione;
+    public:
+        Libreria(string tit, int anPub):titolo(tit), annoPubblicazione(anPub){
         }
-    }
+        void mostraDettagli(){
+        cout<<"Titolo: "<<titolo<<", Anno di Pubblicazione: "<<annoPubblicazione<<endl;
+        }
+        bool isAntico(){
+            int annoCorrente=2023;
+            return (annoCorrente-annoPubblicazione)>=50;
+        }
+        int calcolaAnni() {
+            int annoCorrente=2023;
+            return annoCorrente-annoPubblicazione;
+        }
+};
+
+class Libro:public Libreria{
+    private:
+        int numeroPagine;
+        string genere;
+    public:
+        Libro(string tit, int anPub, int pagine, string gen):Libreria(tit, anPub), numeroPagine(pagine), genere(gen){
+        }
+        bool verificaLunghezza(){
+            return numeroPagine>300;
+        }
+        bool eFantasy(){
+            return genere=="Fantasy";
+        }
+        void calcolaTempoLettura() {
+            int tempoLettura=numeroPagine/30;
+            cout<<"Tempo stimato di lettura: "<<tempoLettura<<" ore"<<endl;
+        }
+};
+
+class Rivista:public Libreria{
+    private:
+        int numeroArticoli;
+        string categoria;
+    public:
+        Rivista(string tit, int anPub, int articoli, string cat):Libreria(tit, anPub), numeroArticoli(articoli), categoria(cat) {}
+        bool haMoltiArticoli(){
+            return numeroArticoli>10;
+        }
+        bool eDiCategoria(string cat){
+            return categoria==cat;
+        }
+        void mostraArticoli(){
+            for(int i=1;i<=numeroArticoli;i++){
+                cout<<"Articolo "<<i<<endl;
+            }
+        }
 };
 
 int main(){
