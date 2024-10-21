69ed04e55bd8239a057d6e903898a555b5ba6779
Marigest <marigest.sadiku@itctosi.va.it>
2024-10-21 17:28:42 +0000

    Modification

--- Esercizi/Manager e dipendenti.cpp
+++ Esercizi/Manager e dipendenti.cpp
@@ -4,35 +4,56 @@ using namespace std;
 class Dipendente{
     protected:
         string nome;
-        double stipendiobase;
+        double stipendioBase;
     public:
-        Dipendente(string no, double stip){
-            nome=no;
-            stipendiobase=stip;
+        Dipendente(string n, double stipBas){
+            nome=n;
+            stipendioBase=stipBas;
         }
         void visualizza(){
-            cout<<"Il dipendente "<<nome<<" ha rivevuto uno stipendio base di "<<stipendiobase<<"."<<endl;
+            cout<<"Il nome del dipendete e' "<<nome<<" con uno stipedendio di "<<stipendioBase<<endl;
         }
-        double salariototale(){
-            
+        double salariotot(double bonus){
+            stipendioBase=stipendioBase+bonus;
+            return stipendioBase;
+        }
+        double pagagiornaliera(int g){
+            return (stipendioBase/g);
         }
 };
 
 class Manager:public Dipendente{
     private:
         double bonus;
-        int numerodipendenti;
+        int numeroDipendenti;
     public:
-        Manager(string no, double stip, double bo, int numdip):Dipendente(no, stip){
+        Manager(string n, double stipBas, double bo, int numdip):Dipendente(n, stipBas){
             bonus=bo;
-            numerodipendenti=numdip;
+            numeroDipendenti=numdip;
         }
         void visualizza(){
-            cout<<"I dipendenti di questa azienda sono: "<<numerodipendenti<<endl;
-            cout<<"Il dipendente "<<nome<<" ha rivevuto uno stipendio base di "<<stipendiobase<<"."<<endl;
+            cout<<"Il numero di dipendenti supervisionati dal manager sono "<<numeroDipendenti<<" con un bonus di "<<bonus<<endl;
         }
-}
+        void supermanager(){
+            if(numeroDipendenti>10){
+                bonus=bonus+(bonus*0.3);
+            }
+        }
+        double stipendioManager(){
+            return stipendioBase+bonus;
+        }
+};  
 
 int main(){
-
+Dipendente dip("Marco", 1700);
+int giorni;
+    dip.visualizza();
+    cout<<"Il salario dei dipendenti con il bonus in piu' e' "<<dip.salariotot(200)<<endl;
+    cout<<"Quanti giorni ha lavorato il dipendente? ";
+    cin>>giorni;
+    cout<<"Il dipendente ha una paga giornaliera di "<<dip.pagagiornaliera(giorni)<<endl;
+Manager man("Carlo", 1700, 200, 15);
+    man.visualizza();
+    man.supermanager();
+    cout<<"Lo stipendio del Manager e' di "<<man.stipendioManager()<<endl; 
 }
